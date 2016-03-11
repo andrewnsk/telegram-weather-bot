9feaf0762782f50b850084ad43b3509d4c78c53a
Andrew Dorokhin <andrewnorilsk@gmail.com>
2016-03-11 22:24:10 +0000

    add  some functionality

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import logging
-from telegram import Updater
+import telegram
+from telegram import Updater, emoji
 
 bot_token = ''
+bot = telegram.Bot(token=bot_token)
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -11,4 +13,56 @@ dispatcher = updater.dispatcher
 
 
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    bot.sendMessage(chat_id=update.message.chat_id, text="Привет! "
+                                                         "я бот и я знаю информацию о погоде в Норильске"
+                                                         ", Краснодаре "
+                                                         "и Сочи" + telegram.Emoji.BLACK_SUN_WITH_RAYS)
+
+
+dispatcher.addTelegramCommandHandler('start', start)
+
+
+
+def echo(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+
+dispatcher.addTelegramMessageHandler(echo)
+
+
+def unknown(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text="А вот нет такой команды " + telegram.Emoji.FACE_WITH_STUCK_OUT_TONGUE_AND_WINKING_EYE)
+
+dispatcher.addUnknownTelegramCommandHandler(unknown)
+
+updater.start_polling()
+'''
+def empty_message(bot, update):
+    """
+    Empty messages could be status messages, so we check them if there is a new
+    group member, someone left the chat or if the bot has been added somewhere.
+    """
+
+    # Keep chatlist
+    chats = db.get('chats')
+
+    if update.message.chat.id not in chats:
+        chats.append(update.message.chat.id)
+        db.set('chats', chats)
+        logger.info("I have been added to %d chats" % len(chats))
+
+    if update.message.new_chat_participant is not None:
+        # Bot was added to a group chat
+        if update.message.new_chat_participant.username == BOTNAME:
+            return introduce(bot, update)
+        # Another user joined the chat
+        else:
+            return welcome(bot, update)
+
+    # Someone left the chat
+    elif update.message.left_chat_participant is not None:
+        if update.message.left_chat_participant.username != BOTNAME:
+            return goodbye(bot, update)
+'''
+
+
