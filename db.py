e3e9cd49a7ef79e8d56d161b9eb4187bc52ef3da
Andrew Dorokhin <andrewnorilsk@gmail.com>
2016-03-12 21:44:09 +0000

    add  some functionality

--- db.py
+++ db.py
@@ -2,11 +2,9 @@ import sqlite3
 import os.path
 import logging
 
-
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     filename='system.log')
-
 
 db_file_name = 'settings.db'
 name = ''
@@ -40,11 +38,33 @@ def open_and_read_db(db_file_name_local):
     db_conn_cursor = db_connection.cursor()
     logging.debug('read data')
     for row in db_conn_cursor.execute('SELECT * FROM bot ORDER BY id LIMIT 1'):
-            print(row)
-            logging.debug(row)
+        print(row)
+        logging.debug(row)
     logging.debug('end data')
     db_connection.close()
 
+
+def get_data_from_db(file, table, data):
+    # Get the latest record from table
+    global result_gd
+    result_gd = ''
+    if not data:
+        data = '*'
+
+    db_connection = sqlite3.connect(file)
+    db_conn_cursor = db_connection.cursor()
+
+    logging.debug('read data')
+
+    for row in db_conn_cursor.execute('SELECT {1} FROM {0} ORDER BY id DESC LIMIT 1'.format(table, data)):
+        # id INTEGER PRIMARY KEY, town TEXT, dtime TEXT, t_value TEXT, h_value INTEGER, w_value TEXT, w_dir TEXT
+        # SELECT * FROM    TABLE WHERE   ID = (SELECT MAX(ID)  FROM TABLE);
+        # SELECT * FROM table ORDER BY column DESC LIMIT 1;
+        result_gd = row
+
+    logging.debug('end data')
+    db_connection.close()
+    return result_gd
 
 if not (os.path.isfile(db_file_name)):
     logging.debug('settings file not found')
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import logging
 import telegram
 from telegram import Updater, emoji
+from db import get_data_from_db
 
-bot_token = ''
+bot_token = '186600990:AAHJU4yx2UwFtMf41buQacWsttj6u6Q7rMo'
 bot = telegram.Bot(token=bot_token)
 
 logging.basicConfig(level=logging.DEBUG,
@@ -22,9 +23,35 @@ def start(bot, update):
 dispatcher.addTelegramCommandHandler('start', start)
 
 
+def info(bot, update):
+    file = 'weather.db'
+    table = 'weather'
+    data = ''
+    weather_data = get_data_from_db(file, table, data)
+
+    text = 'Погода в Норильске ' + telegram.Emoji.AERIAL_TRAMWAY + '\n' + \
+           'Температура:         ' + str(weather_data[3]) + '\n', \
+           'Влажность:           ' + str(weather_data[4]) + ' % \n', \
+           'Ветер:               ' + str(weather_data[5]) + ' ' + str(weather_data[6]) + '\n'
+    text_2 = "тест 2"
+    text_3 = 'тест 3'
+    bot.sendMessage(chat_id=update.message.chat_id, text='Погода в Норильске ' +
+                                                         telegram.Emoji.WHITE_UP_POINTING_INDEX + '\n' +
+                                                         'Температура:          ' +
+                                                         str(weather_data[3]) + '\n' +
+                                                         'Влажность:           ' +
+                                                         str(weather_data[4]) + ' % \n' +
+                                                         'Ветер:               ' + str(weather_data[5]) +
+                                                         ' ' + str(weather_data[6]) + '\n'
+                    )
+
+
+dispatcher.addTelegramCommandHandler('info', info)
+
 
 def echo(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+
 
 dispatcher.addTelegramMessageHandler(echo)
 
@@ -32,6 +59,7 @@ dispatcher.addTelegramMessageHandler(echo)
 def unknown(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id,
                     text="А вот нет такой команды " + telegram.Emoji.FACE_WITH_STUCK_OUT_TONGUE_AND_WINKING_EYE)
+
 
 dispatcher.addUnknownTelegramCommandHandler(unknown)
 
@@ -64,5 +92,3 @@ def empty_message(bot, update):
         if update.message.left_chat_participant.username != BOTNAME:
             return goodbye(bot, update)
 '''
-
-
