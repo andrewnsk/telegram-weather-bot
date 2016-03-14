ceb54ef978930f4c1bbe413f9bef37ea722bec09
Andrew Dorokhin <andrewnorilsk@gmail.com>
2016-03-14 21:23:52 +0000

    add

--- bottle_logic.py
+++ bottle_logic.py
@@ -6,40 +6,52 @@ MALE = '1'
 FEMALE = '0'
 BI = '10'
 
+
 class Bottle:
     def __init__(self):
-        self.males = ["Андрей",
-                      "Александр",
-                      "Антон",
-                      "Борис",
-                      "Данил",
-                      "Денис",
-                      "Евгений",
-                      "Константин",
-                      "Павел"]
+        self.males = [
+            "Андрей",
+            "Александр",
+            "Антон",
+            "Борис",
+            "Данил",
+            "Денис",
+            "Евгений",
+            "Константин",
+            "Павел"
+        ]
 
-        self.females = ["Анна",
-                        "Анастасия",
-                        "Валентина",
-                        "Екатерина",
-                        "Мария" ]
+        self.females = [
+            "Анна",
+            "Анастасия",
+            "Валентина",
+            "Екатерина",
+            "Мария"
+        ]
 
         self.gamers = [
-                        "Андрей",
-                        "Александр",
-                        "Антон",
-                        "Борис",
-                        "Данил",
-                        "Денис",
-                        "Евгений",
-                        "Константин",
-                        "Павел",
-                        "Анна",
-                        "Анастасия",
-                        "Валентина",
-                        "Екатерина",
-                        "Мария"
-                        ]
+            "Андрей",
+            "Александр",
+            "Антон",
+            "Борис",
+            "Данил",
+            "Денис",
+            "Евгений",
+            "Константин",
+            "Павел",
+            "Анна",
+            "Анастасия",
+            "Валентина",
+            "Екатерина",
+            "Мария"
+        ]
+
+        self.phrases = [
+            "нежно целует в щечку",
+            "целует взасос",
+            "шлепает по попке"
+
+        ]
 
         self.db_connection = sqlite3.connect(":memory:")
         self.db_connection.isolation_level = None
@@ -47,7 +59,6 @@ class Bottle:
         self.cur.execute('''CREATE TABLE players
                             (id INTEGER PRIMARY KEY, name TEXT, gender TEXT, flags INTEGER, description TEXT)''')
         self.db_connection.commit()
-
 
     def define_gender(self, name):
         if name not in self.males:
@@ -57,7 +68,6 @@ class Bottle:
         else:
             return BI
 
-
     def players(self, names):
 
         self.db_connection = sqlite3.connect(":memory:")
@@ -65,17 +75,39 @@ class Bottle:
         self.cur = self.db_connection.cursor()
 
         for human in names:
-
-        self.cur.execute('''INSERT INTO bot(name, gender)
+            self.cur.execute('''INSERT INTO bot(name, gender)
                             VALUES(?,?,?,?)''', (human, self.define_gender(human)))
 
+    @staticmethod
+    def rnd(obj):
+        quantity = len(obj)
+        return random.randint(0, quantity - 1)
 
-    @property
-    def rnd(self):
-        quantity = len(self.gamers)
-        return random.randint(1, quantity)
+    def kiss(self):
+        self.rnd(self.phrases)
 
-    def phrases(self):
-        pass
+
+new_game = Bottle()
+kissed = new_game.rnd(new_game.gamers)
+print(kissed)
+gamer = new_game.gamers[kissed]
+print(gamer)
+
+if gamer[-1:] == 'я':
+    gamered = gamer[:len(gamer) - 1] + 'ю'
+elif gamer[-1:] == 'й':
+    gamered = gamer[:len(gamer) - 1] + 'я'
+elif gamer[-1:] == 'а':
+    gamered = gamer[:len(gamer) - 1] + 'у'
+elif gamer[-1:] == 'р':
+    gamered = gamer + 'а'
+
+else:
+    gamered = gamer
+
+print(gamered)
+
+
+
 
 
